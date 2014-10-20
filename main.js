ce6691b4bf16dd89a07f9dd96e2944472d32e7b7
Adam Weiner <adamwineguy@gmail.com>
2014-10-20 11:47:48 -0600

    finished ps5

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 $(document).on('ready', function() {
   
+	var alphabetsoup=function(str){
+		return str.split('').sort().join('');
+		// console.log(str);
+	};
+alphabetsoup('hello');
+
+	
+	var vowelcount=function(str){
+		var vowel=/[aeiouAEIOU]/;
+		var isVowel=reg.test.bind(vowel);
+		return str.split('').filter(isVowel).length;
+	};
+
+
 });
